1662386d4cc1d4f64ff07b5f061494b67e8df87f
Emiliano Tisato <emilianotisato@gmail.com>
2017-12-15 19:42:45 -0300

    Configure user and client model

--- spme-pwa/app/Client.php
+++ spme-pwa/app/Client.php
@@ -6,5 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+        'name',
+        'config',
+        'notes',
+        'secrets'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'config' => 'array',
+        'secrets' => 'array',
+    ];
+
+    /**
+     * Projects for this client
+     *
+     * @return void
+     */
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 }
--- spme-pwa/database/seeds/DatabaseSeeder.php
+++ spme-pwa/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(SeveritiesTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
+
+        if (App::isLocal()) {
+            $this->call(ClientsTableSeeder::class);
+            $this->call(ProjectsTableSeeder::class);
+            $this->call(TicketTableSeeder::class);
+        }
     }
 }
