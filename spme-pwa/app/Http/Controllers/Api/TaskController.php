baff6a2c0f1c3f42446f3329f6d94094058a61d7
Emiliano Tisato <emilianotisato@gmail.com>
2018-01-13 19:13:15 -0300

    Added store method for updates

--- spme-pwa/app/Http/Controllers/Api/TaskController.php
+++ spme-pwa/app/Http/Controllers/Api/TaskController.php
@@ -2,21 +2,48 @@
 
 namespace App\Http\Controllers\Api;
 
+use Auth;
 use App\Task;
 use App\Http\Requests\TaskRequest;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateRequest;
 
 class TaskController extends Controller
 {
+    /**
+     * Get task Collection
+     *
+     * @return void
+     */
     public function getTasks()
     {
         return Task::all();
     }
 
+    /**
+     * Edit individual task fields
+     *
+     * @param [type] $id
+     * @param TaskRequest $request
+     * @return void
+     */
     public function editTaskField($id, TaskRequest $request)
     {
         $task = Task::findOrFail($id);
         $task->update($request->all());
         return $task->fresh();
+    }
+
+    public function postTaskUpdate(UpdateRequest $request)
+    {
+        Auth::user()->updates()->create($request->all());
+
+        $task = Task::findOrFail($request->get('task_id'));
+
+        if ($request->get('closed')) {
+            $task->closeTask();
+        }
+
+        return $task;
     }
 }
