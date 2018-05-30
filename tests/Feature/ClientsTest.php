058e9ae6ed9e727bffbb1e1d1a3308c81919a8db
Emiliano Tisato <emilianotisato@gmail.com>
2018-05-30 14:50:17 -0300

    Refactor helper auth with permissions on test class

--- tests/Feature/ClientsTest.php
+++ tests/Feature/ClientsTest.php
@@ -3,8 +3,6 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Support\Facades\Auth;
-use Spatie\Permission\Models\Permission;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -21,7 +19,16 @@ class ClientsTest extends TestCase
     {
         parent::setUp();
 
-        $this->createAdminUser();
+        $this->authUser()
+        ->withRoles(['admin'])
+        ->withPermissions([
+            'client_create',
+            'client_edit',
+            'client_deactivate',
+            'client_delete',
+            'client_see_secret',
+            'client_add_notes'
+            ]);
 
     }
     /** @test */
@@ -36,27 +43,5 @@ class ClientsTest extends TestCase
         // Then we have returned a new client with the rest of the field by default
         $response->assertSuccessful();
         $response->assertJsonFragment(['name' => $name]);
-    }
-
-    public static function createAdminUser()
-    {
-        $client_create = Permission::create(['name' => 'client_create']);
-        $client_edit = Permission::create(['name' => 'client_edit']);
-        $client_deactivate = Permission::create(['name' => 'client_deactivate']);
-        $client_delete = Permission::create(['name' => 'client_delete']);
-        $client_see_secret = Permission::create(['name' => 'client_see_secret']);
-        $client_add_notes = Permission::create(['name' => 'client_add_notes']);
-        $admin = \Spatie\Permission\Models\Role::create(['name' => 'admin']);
-        $admin->givePermissionTo($client_create);
-        $admin->givePermissionTo($client_edit);
-        $admin->givePermissionTo($client_deactivate);
-        $admin->givePermissionTo($client_delete);
-        $admin->givePermissionTo($client_see_secret);
-        $admin->givePermissionTo($client_add_notes);
-
-        $user = factory(\App\User::class)->create(['name' => 'Admin man']);
-        $user->assignRole('admin');
-
-        Auth::loginUsingId($user->id, true);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,46 @@
 
 namespace Tests;
 
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Permission;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public $authUser;
+
+    public function authUser($name = 'Demo User')
+    {
+        $this->authUser = factory(\App\User::class)->create(['name' => $name]);
+        Auth::loginUsingId($this->authUser->id, true);
+
+        return $this;
+    }
+
+    public function withRoles($roles = [])
+    {
+        foreach($roles as $role) {
+
+            Role::create(['name' => $role]);
+
+            $this->authUser->assignRole($role);
+        }
+
+        return $this;
+    }
+
+    public function withPermissions($permissions = [])
+    {
+        foreach($permissions as $permission) {
+
+            Permission::create(['name' => $permission]);
+
+            $this->authUser->givePermissionTo($permission);
+        }
+
+        return $this;
+    }
 }
